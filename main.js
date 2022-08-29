37f05df9a33e2b11c733f4b631a313cfacfb4a64
Rohith M <rohithmerala@gmail.com>
2022-08-29 17:00:50 +0530

    idk

--- main.js
+++ main.js
@@ -1,4 +1,20 @@
-function setup(){
-    canvas = createCanvas(550, 550);
-    canvas.position(400, 150)
+function setup() {
+  canvas = createCanvas(550, 550);
+  canvas.position(500, 300);
+  video = createCapture(VIDEO);
+  video.size(500, 500);
+  pose_net = ml5.poseNet(video, modelloaded);
+  pose_net.on("pose", gotposes);
+  canvas = createCanvas(550, 550);
+  canvas.position(500, 140);
+}
+
+function modelloaded() {
+  console.log("Pose Net Is Loaded.");
+}
+
+function gotposes(results) {
+  if (results.length > 0) {
+    console.log(results);
+  }
 }
